479b6ff829d3a0734e205d25b3a88a8a8f07eb2d
Makasya <maxytka228@gmail.com>
2025-12-17 20:22:51 +0500

    Add project files.

---
+++ Tyuiu.FisherMA.Sprint7.Project.V9.Lib/DataService.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.FisherMA.Sprint7.Project.V9.Lib
+{
+    public class DataService
+    {
+
+    }
+}
---
+++ Tyuiu.FisherMA.Sprint7.Project.V9.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.FisherMA.Sprint7.Project.V9.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
